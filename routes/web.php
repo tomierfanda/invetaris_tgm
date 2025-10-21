1666f9fe7237a2c1c81fadcc6a1b1aeb358ae915
adl.tomie <adl.tomie@xl.co.id>
2025-10-21 16:03:45 +0700

    fix web.php

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InventarisController;
 
-Route::get('/', function(){ return redirect()->route('inventaris.index'); });
+Route::get('/', function(){ 
+    return redirect()->route('inventaris.index'); 
+});
+
+// Route Resource sudah membuat: .index, .create, .store, .show, .edit, .update, .destroy
 Route::resource('inventaris', App\Http\Controllers\InventarisController::class)->parameters([
+    // Ini bagus untuk memastikan parameter model binding menggunakan nama 'inventaris'
     'inventaris' => 'inventaris'
 ]);
-Route::get('/inventaris/{id}', [InventarisController::class, 'show'])->name('inventaris.show');
+
+// Route Tambahan (Ini unik, jadi aman)
 Route::get('inventaris-search', [InventarisController::class, 'search'])->name('inventaris.search');
 Route::get('inventaris/{id}/print-barcode', [InventarisController::class, 'printBarcode'])->name('inventaris.printBarcode');
 Route::post('inventaris/export', [InventarisController::class, 'export'])->name('inventaris.export');
-
-
