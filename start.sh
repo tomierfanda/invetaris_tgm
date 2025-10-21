e6fc2b9ec82a60e8189baedfb3a46ec7685fdbb6
adl.tomie <adl.tomie@xl.co.id>
2025-10-21 18:56:11 +0700

    fix start shH

--- start.sh
+++ start.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
+
 echo "===== DB Config ====="
 echo "DB_HOST:     $DB_HOST"
 echo "DB_PORT:     $DB_PORT"
 echo "DB_DATABASE: $DB_DATABASE"
 echo "DB_USERNAME: $DB_USERNAME"
 echo "DB_PASSWORD: $DB_PASSWORD"
+php -r "echo 'DB_PASSWORD2: '.getenv('DB_PASSWORD').\"\n\";"
 echo "====================="
 
 echo "Waiting for DB..."
 
 # Tunggu MySQL siap
-until php -r "new PDO('mysql:host='.$_ENV['DB_HOST'].';dbname='.$_ENV['DB_DATABASE'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);" 2>/dev/null; do
-  echo "DB not ready, retry in 3s..."
-  sleep 3
+until php -r "new PDO(
+    'mysql:host='.getenv('DB_HOST').';dbname='.getenv('DB_DATABASE').';port='.getenv('DB_PORT'),
+    getenv('DB_USERNAME'),
+    getenv('DB_PASSWORD')
+);" 2>/dev/null; do
+    echo "DB not ready, retry in 3s..."
+    sleep 3
 done
 
 echo "DB ready! Running migrations..."
@@ -22,5 +28,5 @@ php artisan db:seed
 # Jalankan PHP-FPM
 php-fpm &
 
-# Jalankan Nginx
+# Jalankan Nginx di foreground
 nginx -g "daemon off;"
