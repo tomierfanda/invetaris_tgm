a8d6e788c1f3330a9868d47540b8ba1efb3f0e61
adl.tomie <adl.tomie@xl.co.id>
2025-10-21 18:21:18 +0700

    fix env

--- start.sh
+++ start.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+echo "Waiting for DB..."
+until php -r "new PDO('mysql:host='.$_ENV['DB_HOST'].';dbname='.$_ENV['DB_DATABASE'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);" 2>/dev/null; do
+  sleep 3
+done
+echo "DB ready!"
+
+php artisan migrate --force
+
 echo "Starting PHP-FPM..."
 # Start PHP-FPM
 php-fpm &
