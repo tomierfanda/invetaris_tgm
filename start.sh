fe83b8ce12bbfe36d2587a5e4f47ecee5af34f3d
adl.tomie <adl.tomie@xl.co.id>
2025-10-23 09:14:53 +0700

    fix

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-set -e
-
 echo "🔧 Fixing permissions..."
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
